4a30c183730670716c5a4ab6ecfa2f1f2a55b144
Stephen Yeargin <stephen.yeargin@gmail.com>
2014-08-30 16:23:39 -0500

    Adds Uber time-to-pickup board
    Fixes #2
    
    Requires three new environment variables:
    
    ```
    export UBER_SERVER_TOKEN=4MAm9nWExNm0SKXaI72c_MEbnZ2OjUZBZPz-t58W
    export UBER_LATITUDE=36.162876
    export UBER_LONGITUDE=-86.78136
    ```

---
+++ jobs/uber.rb
@@ -0,0 +1,38 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+UBER_SERVER_TOKEN = ENV["UBER_SERVER_TOKEN"]
+UBER_LATITUDE     = ENV["UBER_LATITUDE"]
+UBER_LONGITUDE    = ENV["UBER_LONGITUDE"]
+
+SCHEDULER.every '5m', :first_in => 0 do |job|
+
+  begin
+    uri = URI("https://api.uber.com/v1/estimates/time?start_latitude=#{UBER_LATITUDE}&start_longitude=#{UBER_LONGITUDE}")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+    request.add_field("Authorization", "Token #{UBER_SERVER_TOKEN}")
+
+    response = http.request(request)
+    uber_response = JSON.parse(response.body)
+
+    pickup_times = []
+    uber_response['times'].map do |item|
+      estimate = (item['estimate']/60).ceil
+      pickup_times << {
+        label: item['localized_display_name'],
+        value: "#{estimate} minute(s)"
+      }
+    end
+
+    event_name = 'uber_pickup_time'
+    send_event(event_name, { items: pickup_times })
+
+  rescue
+    puts "\e[33mYou need to add your UBER_SERVER_TOKEN and define a latitude and longitude.\e[0m"
+  end
+
+end
