ba76264f9c4a9147a8f101c4f32008fca73d0ed4
Stephen Yeargin <stephen.yeargin@gmail.com>
2014-08-30 20:34:09 -0500

    Make it 10, clean up bullshit

--- jobs/fitbit_leaderboard.rb
+++ jobs/fitbit_leaderboard.rb
@@ -1,6 +1,7 @@
 fitbit = Fitbit.new
 
 SCHEDULER.every "15m", first_in: 0 do |job|
+  puts fitbit.leaderboard.inspect
   if fitbit.errors?
     send_event "fitbit_leaderboard", { error: fitbit.error }
   else
