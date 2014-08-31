49c472af08bb8318fe0bb961aa1d6b0aefc8b272
Stephen Yeargin <stephenyeargin@users.noreply.github.com>
2014-08-30 20:34:41 -0500

    Update fitbit_leaderboard.rb

--- jobs/fitbit_leaderboard.rb
+++ jobs/fitbit_leaderboard.rb
@@ -1,7 +1,6 @@
 fitbit = Fitbit.new
 
 SCHEDULER.every "15m", first_in: 0 do |job|
-  puts fitbit.leaderboard.inspect
   if fitbit.errors?
     send_event "fitbit_leaderboard", { error: fitbit.error }
   else
