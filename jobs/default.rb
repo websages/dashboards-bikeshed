ce2666cea22bd777d7d1063ea4814898d1942c25
Stephen Yeargin <stephenyeargin@users.noreply.github.com>
2014-09-02 18:22:36 -0500

    quit dripping shit everywhere

--- jobs/default.rb
+++ jobs/default.rb
@@ -3,12 +3,9 @@ SCHEDULER.every '5m', :first_in => 0 do |job|
   # dashboard_list = Dir["dashboards/*.erb"]
   dashboard_list = Dir["dashboards/*.erb"]
 
-  puts dashboard_list.inspect
-
   dashboards = []
   for dashboard in dashboard_list
     next if dashboard === 'dashboards/layout.erb'
-    puts dashboard
     dashboard = dashboard.match(/^dashboards\/(.*)\.erb$/)[1]
     dashboards << {
       label: dashboard,
