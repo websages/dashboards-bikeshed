4aa13d8d5815cf7ce08c2f1ed7246cd5a060cd03
Stephen Yeargin <stephen.yeargin@gmail.com>
2014-09-02 18:09:14 -0500

    Add a default dashboard
    It's a pain in the ass not to have one to browse around with.

---
+++ jobs/default.rb
@@ -0,0 +1,21 @@
+SCHEDULER.every '10s', :first_in => 0 do |job|
+
+  # dashboard_list = Dir["dashboards/*.erb"]
+  dashboard_list = Dir["dashboards/*.erb"]
+
+  puts dashboard_list.inspect
+
+  dashboards = []
+  for dashboard in dashboard_list
+    next if dashboard === 'dashboards/layout.erb'
+    puts dashboard
+    dashboard = dashboard.match(/^dashboards\/(.*)\.erb$/)[1]
+    dashboards << {
+      label: dashboard,
+      value: "<a href='#{dashboard}'>Go</a>"
+    }
+  end
+
+  send_event('dashboards', { items: dashboards })
+
+end
