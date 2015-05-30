0872488e6c9cdad4e1d961a146c2f532e274dc11
Stephen Yeargin <stephen.yeargin@gmail.com>
2015-05-29 23:56:43 -0500

    Fixup BTC

--- jobs/bitcoin.rb
+++ jobs/bitcoin.rb
@@ -4,18 +4,25 @@ SCHEDULER.every '10m' do
   last_price = current_price
   current_price = get_bitcoin('USD')
 
-  send_event('bitcoin', { current: current_price, last: last_price })
+  send_event(
+    'bitcoin',
+    current: current_price,
+    last: last_price,
+    prefix: '$'
+  )
 end
 
-def get_bitcoin(market='USD')
-    uri = URI("https://api.bitcoinaverage.com/ticker/global/#{market}/")
-    http = Net::HTTP.new(uri.host, uri.port)
-    http.use_ssl = true
-    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Get.new(uri.request_uri)
+##
+# Get Bitcoin Price
+def get_bitcoin(market = 'USD')
+  uri = URI("https://api.bitcoinaverage.com/ticker/global/#{market}/")
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  request = Net::HTTP::Get.new(uri.request_uri)
 
-    response = http.request(request)
-    bitcoin_response = JSON.parse(response.body)
+  response = http.request(request)
+  bitcoin_response = JSON.parse(response.body)
 
-    bitcoin_response['last']
+  bitcoin_response['last']
 end
