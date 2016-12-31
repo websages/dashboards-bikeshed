b6833b5548fe5cd5359de8b93cd1758f150c84c1
James S. White <jameswhite@github.com>
2016-12-31 14:41:29 -0600

    put it back now that it's getting 200 and not 404

--- jobs/forcast.rb
+++ jobs/forcast.rb
@@ -15,16 +15,13 @@ forecast_location_long = "-86.7816"
 # "uk" - SI w. windSpeed in mph
 forecast_units = "si"
 
-#SCHEDULER.every '5m', :first_in => 0 do |job|
+SCHEDULER.every '5m', :first_in => 0 do |job|
   http = Net::HTTP.new("api.forecast.io", 443)
   http.use_ssl = true
   http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-  puts "api.forecast.io/forecast/#{forecast_api_key}/#{forecast_location_lat},#{forecast_location_long}?units=#{forecast_units}"
   response = http.request(Net::HTTP::Get.new("/forecast/#{forecast_api_key}/#{forecast_location_lat},#{forecast_location_long}?units=#{forecast_units}"))
-  pp response
   forecast = JSON.parse(response.body)
   forecast_current_temp = forecast["currently"]["temperature"].round
   forecast_hour_summary = forecast["hourly"]["summary"]
-  puts "forecast temperature: #{forecast_current_temp}&deg;, hour: #{forecast_hour_summary}"
-  # send_event('forecast', { temperature: "#{forecast_current_temp}&deg;", hour: "#{forecast_hour_summary}"})
-#end
+  send_event('forecast', { temperature: "#{forecast_current_temp}&deg;", hour: "#{forecast_hour_summary}"})
+end
