5daedb1e69ded8200e641b226687edee33214c21
Stephen Yeargin <stephen.yeargin@gmail.com>
2017-10-13 22:30:07 -0500

    Fix Bitcoin, Add Etherium
    Fixes #45

--- jobs/bitcoin.rb
+++ jobs/bitcoin.rb
@@ -1,28 +1,41 @@
-current_price = 0
+# Defaults
+btc_current_price = 0
+eth_current_price = 0
 
-SCHEDULER.every '2m' do
-  last_price = current_price
-  current_price = get_bitcoin('USD')
+SCHEDULER.every '10m', first_in: 0 do
+  # Bitcoin
+  btc_last_price = btc_current_price
+  btc_current_price = get_coinbase_data('BTC')
+
+  # Etherium
+  eth_last_price = eth_current_price
+  eth_current_price = get_coinbase_data('ETH')
 
   send_event(
     'bitcoin',
-    current: current_price,
-    last: last_price,
+    current: btc_current_price,
+    last: btc_last_price,
+    prefix: '$'
+  )
+
+  send_event(
+    'etherium',
+    current: eth_current_price,
+    last: eth_last_price,
     prefix: '$'
   )
 end
 
 ##
-# Get Bitcoin Price
-def get_bitcoin(market = 'USD')
-  uri = URI("https://api.bitcoinaverage.com/ticker/global/#{market}/")
+# Get Coinbase Prices
+def get_coinbase_data(coin)
+  uri = URI("https://api.coinbase.com/v2/exchange-rates?currency=#{coin}")
   http = Net::HTTP.new(uri.host, uri.port)
   http.use_ssl = true
-  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
   request = Net::HTTP::Get.new(uri.request_uri)
 
   response = http.request(request)
   bitcoin_response = JSON.parse(response.body)
 
-  bitcoin_response['last']
+  bitcoin_response['data']['rates']['USD']
 end
