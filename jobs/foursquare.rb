fa95cefd7fb6be69559701f9ec31413b20f1270a
Stephen Yeargin <stephen.yeargin@gmail.com>
2017-10-18 21:15:39 -0500

    Finish up

--- jobs/foursquare.rb
+++ jobs/foursquare.rb
@@ -15,20 +15,11 @@ class RecentFoursquare
     recent_checkins = client.recent_checkins
     locations = []
     for row in recent_checkins[0...10]
-      time = Time.at(row['createdAt']).to_i
+      time = Time.at(row['createdAt']).strftime('%-m/%-d @ %l:%M %p')
       locations << {
-        label: "#{row['user']['firstName']} #{row['user']['lastName']} @ #{row['venue']['name']} - <em style='color:white;''>#{humanize time} ago</em>",
+        label: "#{row['user']['firstName']} #{row['user']['lastName']} @ #{row['venue']['name']} - <em style='color:white;''>#{time}</em>",
       }
     end
     return locations
-  end
-
-  def humanize secs
-    [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
-      if secs > 0
-        secs, n = secs.divmod(count)
-        "#{n.to_i} #{name}"
-      end
-    }.compact.reverse.join(' ')
   end
 end
